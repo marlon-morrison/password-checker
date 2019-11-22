06a86de9e78c6947c59b62ddaa0d6e92385b9499
marlon-morrison <52177393+marlon-morrison@users.noreply.github.com>
2019-11-22 09:07:41 -0600

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,42 @@
+// Password must have at least 8 characters but no more than 20;
+// Must contain at least one special charcter(!, @, #, $, %, &); 
+// Must contain at least one uppercase letter; 
+// must contain at least one lowercase letter; 
+// Must contain at least one number; 
+// cannot contain spaces
+
+function confirm() {
+    event.preventDefault();
+    var password1 = (document.getElementById("password1").value)
+    var password2 = (document.getElementById("password2").value)
+    //var regexp = ;
+    if (password1.length > 20) {
+        alert("Your password needs a maximum of 20 characters")
+      } else if (password1.search(/[a-z]/) < 0) {
+        alert("Your password needs a lower case letter")
+      } else if(password1.search(/[A-Z]/) < 0) {
+        alert("Your password needs an uppser case letter")
+      } else  if (password1.search(/[0-9]/) < 0) {
+        alert("Your password needs a number")
+      } else if (password1.search(/[^\S]/) > 0) {
+        alert("Your password contains spaces")
+      } else if (password1.search(/[!|@|#|$|%|&]/) < 0) {
+        alert("Your password need at least one special charcter")
+      } 
+        else if (password1 != password2) {
+        alert ("\nPassword did not match: Please try again...") 
+        return false; 
+      } else {
+        location = "https://marlon-morrison.github.io/age-confirm/"
+      }
+    };
+
+function showPassword() {
+    if (password1.type == "password") {
+        password1.type = "text";
+        password2.type = 'text'
+      } else {
+        password1.type = "password"
+        password2.type = "password"
+      }
+    };
